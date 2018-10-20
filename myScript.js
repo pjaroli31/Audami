0911e60ca6243a65a69753e229cad062855898d0
AjiteshGupta13 <ajiteshgupta13@gmail.com>
2018-10-20 16:58:59 +0530

    added draw functionality

---
+++ myScript.js
@@ -0,0 +1,19 @@
+// create a wrapper around native canvas element (with id="c")
+var canvas = new fabric.Canvas('myCanvas');
+
+// create a rectangle object
+var rect = new fabric.Rect({
+    left: 100,
+    top: 100,
+    fill: 'red',
+    width: 20,
+    height: 20
+});
+
+
+function listener(name) {
+    if (name == 'circle') {
+        canvas.add(rect);
+    }
+}
+// "add" rectangle onto canvas
