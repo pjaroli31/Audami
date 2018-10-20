cf4c489036a9fa5932df331ab20c3c941ac83af2
pjaroli31 <pulkitjaroli@gmail.com>
2018-10-20 17:59:16 +0530

    Added speech js file and passing functionality

---
+++ voicedetect.js
@@ -0,0 +1,24 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+var stopRecog = new SpeechRecognition();
+var str;
+function startRecord() {
+    const recognition = new SpeechRecognition();
+    stopRecog = recognition;
+    recognition.interimResults = true;
+    
+    
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+            .map(result => result[0])
+            .map(result => result.transcript)
+            .join('');
+      
+        if (e.results[0].isFinal) {
+            console.log(transcript);
+            acceptString(transcript);
+            recognition.stop();
+        }
+    });
+    //recognition.addEventListener('end', recognition.start);
+    recognition.start();
+}
